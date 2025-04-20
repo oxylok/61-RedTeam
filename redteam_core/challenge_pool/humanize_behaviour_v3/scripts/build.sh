8972f21d0e78eb5343020c3a78a31f4c0c1b3036
Batkhuu Byambajav <batkhuu10@gmail.com>
2025-04-20 12:34:48 +0900

    fix: update humanize behaviour image repository name

--- redteam_core/challenge_pool/humanize_behaviour_v3/scripts/build.sh
+++ redteam_core/challenge_pool/humanize_behaviour_v3/scripts/build.sh
@@ -26,7 +26,7 @@ fi
 # Load from envrionment variables:
 # BASE_IMAGE
 IMG_REGISTRY=${IMG_REGISTRY:-}
-IMG_REPO=${PROJECT_SLUG:-humanize_behaviour_v1}
+IMG_REPO=${PROJECT_SLUG:-humanize_behaviour}
 IMG_VERSION=${IMG_VERSION:-$(./scripts/get-version.sh)}
 IMG_SUBTAG=${IMG_SUBTAG:-}
 IMG_PLATFORM=${IMG_PLATFORM:-$(uname -m)}
